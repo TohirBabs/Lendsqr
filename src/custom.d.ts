b628ce830f99d1995ae0e63894bc32512d5de820
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-01 16:35:39 +0100

    Modify Login page to be responsive to mobile devices

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+// declare module "*.svg" { const content: React.FunctionComponent<React.SVGAttributes<SVGAElement>>; export default content};
+declare module "*.svg" { const content: string; export default content};
+// declare module "*.jpg";
+// declare module "*.jpeg";
+// declare module "*.png";
+
+
